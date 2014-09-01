967db80b688e6f5fabef2d6986b45bd73e55ec2c
Yukio Mizuta <untidyhair@gmail.com>
2014-08-31 22:31:12 -0700

    Add YARD doc

--- lib/service_object/base.rb
+++ lib/service_object/base.rb
@@ -1,5 +1,6 @@
 module ServiceObject
   class Base
+    # @return [ServiceObject::Errors] Errors object of the current service
     attr_reader :errors
 
     def initialize
@@ -7,10 +8,14 @@ module ServiceObject
       @errors = Errors.new
     end
 
+    # Error messages of the service process so far
+    # @return [Array] Array of error messages
     def error_messages
       @errors.full_messages
     end
 
+    # Check if the service process is going well or not so far
+    # @return [true, false]
     def result
       @result && @errors.empty?
     end
--- lib/service_object/errors.rb
+++ lib/service_object/errors.rb
@@ -2,6 +2,7 @@ module ServiceObject
   require 'delegate'
 
   class Errors < Delegator
+    # @return [Array] Messages of the current errors
     attr_reader :messages
 
     def initialize
@@ -12,16 +13,24 @@ module ServiceObject
       @messages
     end
 
+    # Return all the current error messages
+    # @return [Array] Messages of the current errors
     def full_messages
       messages
     end
 
+    # Add a new error message to the current error messages
+    # @param message [String] New error message to add
     def add(message)
       @messages << message
     end
 
     class << self
 
+      # Change activemodel errors into a string to be added to service errors
+      # @param active_model [ActiveModel] ActiveModel Object
+      #   whose error messages are to be flattened
+      # @return [String] Flattened string error message
       def flattened_active_model_error(active_model)
         "#{active_model.class}: #{active_model.errors.full_messages.join(', ')}"
       end
