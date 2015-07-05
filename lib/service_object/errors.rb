d5b0e516af2488e7a75569c6e104e5475f47478e
Yukio Mizuta <untidyhair@gmail.com>
2015-07-05 14:20:55 -0700

    Enhance Errors with #to_xml and #as_json

--- lib/service_object/errors.rb
+++ lib/service_object/errors.rb
@@ -1,8 +1,27 @@
-module ServiceObject
-  require 'delegate'
+require 'active_support/core_ext/array/conversions'
+require 'delegate'
 
-  # Responsible for containing, providing and adding errors on service layer.
-  # Also provides a utility to stringify active model errors.
+module ServiceObject
+
+  # Provides a customized +Array+ to contain errors that happen in service layer.
+  # Also provides a utility APIs to handle errors well in controllers.
+  # (All array methods are available by delegation, too.)
+  #   errs = ServiceObject::Errors.new
+  #   errs.add 'Something is wrong.'
+  #   errs.add 'Another is wrong.'
+  #   errs.messages
+  #   => ['Something is wrong.','Another is wrong.']
+  #   errs.full_messages
+  #   => ['Something is wrong.','Another is wrong.']
+  #   errs.to_xml
+  #   => "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<errors>\n  <error>Something is wrong.</error>\n
+  #  <error>Another is wrong.</error>\n</errors>\n"
+  #   errs.empty?
+  #   => false
+  #   errs.clear
+  #   => []
+  #   errs.empty?
+  #   => true
   class Errors < Delegator
     # @return [Array<String>] Messages of the current errors
     attr_reader :messages
@@ -11,20 +30,43 @@ module ServiceObject
       @messages = []
     end
 
-    def __getobj__
+    # @private
+    def __getobj__ # :nodoc:
       @messages
     end
 
-    # Return all the current error messages
+    # Returns all the current error messages
     # @return [Array<String>] Messages of the current errors
     def full_messages
       messages
     end
 
-    # Add a new error message to the current error messages
+    # Adds a new error message to the current error messages
     # @param message [String] New error message to add
     def add(message)
       @messages << message
+    end
+
+    # Generates XML format errors
+    #   errs = ServiceObject::Errors.new
+    #   errs.add 'Something is wrong.'
+    #   errs.add 'Another is wrong.'
+    #   errs.to_xml
+    #   =>
+    #   <?xml version=\"1.0\" encoding=\"UTF-8\"?>
+    #    <errors>
+    #      <error>Something is wrong.</error>
+    #      <error>Another is wrong.</error>
+    #    </errors>
+    # @return [String] XML format string
+    def to_xml(options={})
+      super({ root: "errors", skip_types: true }.merge!(options))
+    end
+
+    # Generates duplication of the message
+    # @return [Array<String>]
+    def as_json
+      messages.dup
     end
   end
 end
--- spec/service_object/errors_spec.rb
+++ spec/service_object/errors_spec.rb
@@ -27,4 +27,27 @@ describe ServiceObject::Errors do
       expect(subject.full_messages).to eq subject.messages
     end
   end
+
+  describe '#to_xml' do
+    it 'returns a xml format string' do
+      subject.add 'Something went wrong.'
+      subject.add 'Another error happened.'
+      expect(subject.to_xml).to eq <<-XML
+<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<errors>
+  <error>Something went wrong.</error>
+  <error>Another error happened.</error>
+</errors>
+      XML
+    end
+  end
+
+  describe '#as_json' do
+    before { subject.add 'Test error happened' }
+    it 'returns messages as an Array' do
+      expect(subject.as_json).to eq subject.messages
+      expect(subject.as_json).to be_a Array
+    end
+  end
+
 end
